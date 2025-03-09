27336509234d12a6f8882e26a7bdd6df46bd0293
code-by-will <will.code021@gmail.com>
2025-03-09 18:13:56 -0400

    - Added sort_lettersDict(lettersDict) function to sort the dictionary from the count_characters(bookText) function - Added gen_report(filePath) to main.py and changed main() to just establish the file path, and print a generated report with that file path

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from stats import get_num_words
-from stats import count_characters
+from stats import *
 
 def get_book_text(filePath):
     with open(filePath) as f:
         fileContents = f.read()
     return fileContents
 
+def gen_report(filePath):
+    bookText = get_book_text(filePath)
+    report = "============ BOOKBOT ============\n"
+    report +="Analyzing book found at books/frankenstein.txt...\n"
+    report +="----------- Word Count ----------\n"
+    report +="Found "
+    report += str(get_num_words(bookText)) 
+    report += " total words\n"
+    report += "--------- Character Count -------\n"
+    charDict = count_characters(bookText)
+    charDictList = sort_lettersDict(charDict)
+    for dictList in charDictList:
+        if dictList["char"].isalpha():
+            report += dictList["char"]
+            report += ": "
+            report += str(dictList["count"])
+            report += "\n"
+    '''
+    for char in charDict:
+        if char.isalpha():
+            report += char
+            report += ": "
+            report += str(charDict[char])
+            report += "\n"
+    '''
+    report += "============= END ==============="
+    return report
+
 def main():
     frankFilePath = "./books/frankenstein.txt"
-    num_words = get_num_words(get_book_text(frankFilePath))
-    print(f"{num_words} words found in the document")
-
-    numCharacters = count_characters(get_book_text(frankFilePath))
-    for char, count in numCharacters.items():
-        print(f"'{char}': {count}")
-
+    print(gen_report(frankFilePath))
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,15 @@ def count_characters(bookText):
         else:
             lettersDict[c_lower] = 1
     return lettersDict
+
+def sort_by_count(charDict):
+    return charDict["count"]
+
+def sort_lettersDict(lettersDict):
+    lettersDictList = []
+    for k in lettersDict:
+        char = k
+        count = lettersDict[k]
+        lettersDictList.append({"char" : char, "count" : count})
+    lettersDictList.sort(reverse = True, key=sort_by_count)
+    return lettersDictList
