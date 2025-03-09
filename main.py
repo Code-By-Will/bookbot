4455821624fa570993aa4a7396a4e44ce4dc3664
code-by-will <will.code021@gmail.com>
2025-03-09 14:58:08 -0400

    - Added count_characters(bookText) to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import count_characters
 
 def get_book_text(filePath):
     with open(filePath) as f:
@@ -6,7 +7,12 @@ def get_book_text(filePath):
     return fileContents
 
 def main():
-    num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
+    frankFilePath = "./books/frankenstein.txt"
+    num_words = get_num_words(get_book_text(frankFilePath))
     print(f"{num_words} words found in the document")
+
+    numCharacters = count_characters(get_book_text(frankFilePath))
+    for char, count in numCharacters.items():
+        print(f"'{char}': {count}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_num_words(bookText):
     wordsList = bookText.split()
     return len(wordsList)
+
+def count_characters(bookText):
+    lettersDict = {}
+    for c in bookText:
+        c_lower = c.lower()
+        # If character already exists in dictionary, increment count
+        # Otherwise, add it with count of 1
+        if c_lower in lettersDict:
+            lettersDict[c_lower] += 1
+        else:
+            lettersDict[c_lower] = 1
+    return lettersDict
