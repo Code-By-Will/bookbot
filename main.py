37f1856f21c4c890a8d1b6aab7d6f746fc85f66e
code-by-will <will.code021@gmail.com>
2025-03-09 21:05:27 -0400

    - Added comments to the python code

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from stats import *
-import sys
+from stats import * 
+import sys #Importing this mostly for sys.argv so we can take arguments and make the code more versatile
 
 def get_book_text(filePath):
-    with open(filePath) as f:
+    with open(filePath) as f: #We use 'with' because it means that it will close out of the file after the with block
         fileContents = f.read()
     return fileContents
 
 def gen_report(filePath):
+    '''
+    This function takes a file path, and generates a report, using multiple different functions to filter and sort data
+    The data is the text of a book, the file path of which is given as an argument when running the program
+    '''
     bookText = get_book_text(filePath)
     report = "============ BOOKBOT ============\n"
     report +="Analyzing book found at "
@@ -17,6 +21,13 @@ def gen_report(filePath):
     report += str(get_num_words(bookText)) 
     report += " total words\n"
     report += "--------- Character Count -------\n"
+    '''
+    For this information, we need to get the count_characters dictionary, and sort / format it with sort_lettersDict
+    Then, we loop over the list of dictionaries, and pull the data we want, in the format we want it to add it to our
+        report. 
+    We want it to be in the format of the character "char" and the number of times that character appears in the text "count".
+    We want it to be in the format of "char: count"
+    '''
     charDict = count_characters(bookText)
     charDictList = sort_lettersDict(charDict)
     for dictList in charDictList:
@@ -28,12 +39,12 @@ def gen_report(filePath):
     report += "============= END ==============="
     return report
 
-def main():
-    argv = sys.argv
-    if len(argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
+def main(): 
+    argv = sys.argv #Assigning the input to a list variable
+    if len(argv) != 2: #We need the name of the python file to run, and the file path to the text we want to use as input
+        print("Usage: python3 main.py <path_to_book>") #shows the correct way to use this program
+        sys.exit(1) #Gives an exit code of 1 to indicate a failure
     else:
-        filePath = argv[1]
-        print(gen_report(filePath))
+        filePath = argv[1] 
+        print(gen_report(filePath)) #prints our generated report using the gen_report() function.
 main()
--- stats.py
+++ stats.py
@@ -1,27 +1,48 @@
-def get_num_words(bookText):
-    wordsList = bookText.split()
-    return len(wordsList)
+def get_num_words(bookText): #A function that gets how many words there are in the provided text
+    wordsList = bookText.split() #We want to take advantage of the len() list function, so we split the text into a list of words
+    return len(wordsList) 
 
 def count_characters(bookText):
-    lettersDict = {}
+    '''
+    With this function we want to accomplish the following:
+        1. take the raw text provided, and loop over the letters
+        2. create a dictionary of key : value pairs - character : how_many_times_that_character_appears
+        3. return that dictionary    
+    '''
+    lettersDict = {} #This is what we will output at the end of the function
+    #1:
     for c in bookText:
-        c_lower = c.lower()
-        # If character already exists in dictionary, increment count
-        # Otherwise, add it with count of 1
+        c_lower = c.lower() #We don't care about case for this
+        
+        #2:
+        '''
+            - If character already exists in dictionary, increment count
+            - Otherwise, add it with count of 1
+        '''
         if c_lower in lettersDict:
             lettersDict[c_lower] += 1
         else:
             lettersDict[c_lower] = 1
+    #3:
     return lettersDict
 
-def sort_by_count(charDict):
+def sort_by_count(charDict): #Helper function for the .sort() method we use in sort_lettersDict()
     return charDict["count"]
 
 def sort_lettersDict(lettersDict):
+    '''
+    In order to best format our data we need to do a few things:
+        1. We need a list of dictionaries in order to use the .sort() method, so we need to create one in the proper formatting
+            using the data from the dictionary we created with the count_characters() function
+        2. Use the .sort() method to organize and sort the data by the number of times the letter was used in the text
+    '''
     lettersDictList = []
-    for k in lettersDict:
+    #1:
+    for k in lettersDict: #loop over the provided dictionary, using k to store the key for each step of the program
         char = k
         count = lettersDict[k]
-        lettersDictList.append({"char" : char, "count" : count})
-    lettersDictList.sort(reverse = True, key=sort_by_count)
-    return lettersDictList
+        lettersDictList.append({"char" : char, "count" : count})#Creates a dictionary, and appends it to a list
+    #2:
+    lettersDictList.sort(reverse = True, key=sort_by_count) #Actually sorting the data! (using the .sort() method)
+
+    return lettersDictList #returning the final product
